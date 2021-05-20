02b5fe8284334ebea096ee0930325ba15c9c10ab
avin99 <avin.rathore@gmail.com>
2021-05-20 14:52:00 +0530

    code submit

---
+++ uva problems/the drunk jailer optimised2(b).cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define PB emplace_back
+#define MP make_pair
+#define F first
+#define S second
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+    IOS;
+    ll n, m, i, j, t;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+
+        cout << (ll)sqrt(n) << endl;
+    }
+    return 0;
+}
